734f6657b7f186730cbcdfae2b96240cc6a22869
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-04-19 17:11:25 +0500

    Add files via upload

---
+++ javaScriptFolder/folder6/script.js
@@ -0,0 +1,72 @@
+// let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// console.log(arr);
+// console.log(arr[4]);
+
+// let list = ["Hussain", ["Teacher", "Web"]];
+// console.log("Sir Abrar", list[0], "Is A", list[1][0], "Of", list[1][1];
+
+//ARRAY METHODS..................
+// let a = ["KIRAN", 19, "WEB DEVELOPER",];
+//  console.log(a.pop());
+//  console.log(a);
+
+// console.log(a.push("FRONT-END DEVELOPER"));
+// console.log(a);
+
+// console.log(a.unshift("Mrs."));
+// console.log(a);
+
+// console.log(a.shift());
+// console.log(a);
+
+// let fruits = ["Mango", "Orange", "Pineapple", ["Kiwi", "Banana", "Apple"]];
+// // fruits.push("app");
+// console.log(fruits);
+// console.log(fruits);
+// console.log(fruits.pop());
+// console.log(fruits);
+// console.log(fruits.pop());
+// console.log(fruits);
+// console.log(fruits.pop());
+// console.log(fruits);
+
+//TASK...........
+// let fruits = [];
+// console.log(fruits);
+// fruits.push("Apple");
+// fruits.push("Mango");
+// fruits.push("Banana");
+// fruits.push("Orange");
+// fruits.push("Kiwi");
+// fruits.push("PineApple");
+// console.log(fruits);
+// // console.log(fruits[0]);
+// console.log(fruits.shift())
+// console.log(fruits);
+
+// console.log(fruits.unshift("Coconut", "Watermelon"));
+// console.log(fruits);
+
+const human = {
+  fName: "Kiran",
+  rollNumber: 30,
+  eyesColor: "Black",
+  education: "Intermediate",
+  age: 19,
+  weight: 42,
+};
+
+console.log(human["fName"]);
+console.log(human.fName);
+console.log(human);
+
+let fruit = {
+  name: "Mango",
+  color : 'Yellow',
+  size: "Big",
+  taste: "Sweet",
+  quantity: 12,
+};
+console.log(fruit);
+fruit.quantity = 21;
+console.log(fruit);
