22b6f9dc540a93fe907ff62ef2ac30bc443da044
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-04-19 17:06:26 +0500

    Add files via upload

---
+++ javaScriptFolder/folder3/script.js
@@ -0,0 +1,216 @@
+// // FIRST TASK...............
+// let mobile = "IPhone";
+// if (mobile == "IPhone") {
+//   console.log("I AM RICH BECAUSE I HAVE IPHONE");
+// }
+
+// // SECOND TASK....................
+// let car = "Corolla";
+// if (car == "Mehran") {
+//   console.log("FUEL EFFICIENT");
+// }
+// if (car == "Cultus") {
+//   console.log("Kalh lagi aw");
+// }
+// if (car == "Corolla") {
+//   console.log("Dadho thi piye");
+// }
+
+// // THIRD TASK................
+// let gender = "d";
+// if (gender == "m") {
+//   console.log("You Are A Male");
+// }
+// if (gender == "f") {
+//   console.log("You Are A Female");
+// }
+// if (
+//   gender == "a" ||
+//   "b" ||
+//   "c" ||
+//   "d" ||
+//   "e" ||
+//   "g" ||
+//   "h" ||
+//   "i" ||
+//   "j" ||
+//   "k" ||
+//   "l" ||
+//   "m" ||
+//   "n" ||
+//   "o" ||
+//   "p" ||
+//   "q" ||
+//   "r" ||
+//   "s" ||
+//   "t" ||
+//   "u" ||
+//   "v" ||
+//   "w" ||
+//   "x" ||
+//   "y" ||
+//   "z"
+// ) {
+//   console.log("I Don't Know What You Are.");
+// }
+
+// // FOURTH TASK................
+// let age = 50;
+// if (age == 5) {
+//   console.log("YOU ARE A CHILD");
+// }
+// if (age == 18) {
+//   console.log("YOU ARE A TEENAGER");
+// }
+// if (age == 50) {
+//   console.log("YOU ARE OLD");
+// }
+
+// //TASK FIVE
+// let marks = 93;
+// if (marks < 50) {
+//   console.log("YOUR MARKS ARE", marks, ". AND YOU ARE FAIL");
+// }
+// if (marks == 80) {
+//   console.log("YOUR MARKS ARE", marks, ". AND YOUR GRADE IS A1 GRADE. ");
+// }
+// if (marks > 80) {
+//   console.log("YOUR MARKS ARE", marks, ". AND YOU GOT 1ST POSITION");
+// }
+
+// TASK SIX............
+// let status = "Not Alive";
+// if (status == "Alive") {
+//   console.log("Zinda Hy");
+// } else {
+//   console.log("Punhcha Jannat Mai");
+// }
+
+// TASK SEVEN..............
+// let restaurant = "Close";
+// if (restaurant == "Open") {
+//   console.log("ACHO MANI KHAO.");
+// } else {
+//   console.log("SUBHANE ACHJO.");
+// }
+
+//TASK EIGGHT................
+// let status = "fail"
+// if( status="pass"){
+//     console.log("Congratulations")
+// }
+// else{
+//     console.log("Better next time")
+// }
+
+//TASK NINE...............
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <script src="ifelse.js">
+
+//     </script>
+// </head>
+// <body>
+
+// </body>
+// </html>
+
+//TASK TEN............
+// mobile="iphone";
+// if(mobile=="Iphone"){
+//      console.log("wado amer aan putr");
+
+// }
+
+// if(mobile=="samsung"){
+//     console.log("wah ji quality aah aaye!");
+// }
+
+//TASK 11................
+// let num = 23;
+// if (num %2 == 0) {
+//   console.log(num,"IS AN EVEN NUMBER");
+// }
+// else{
+//   console.log(num,"IS AN ODD NUMBER");
+
+// }
+
+//TASK 12........
+// let num = -12;
+// if (num >= 0) {
+//   console.log(num, "IS A POSITIVE NUMBER");
+// } else {
+//   console.log(num, "IS A NEGATIVE NUMBER");
+
+// }
+
+//TASK 13........
+// let age = 15;
+// if (age >= 18) {
+//   console.log("YOUR AGE IS ", age, "AND YOU CAN VOTE.");
+// } else {
+//   console.log("YOUR AGE IS ", age, "AND YOU CANNOT VOTE.");
+
+// }
+
+// //TASK 14......
+// var classes = "CIT";
+// if (classes == "CIT") {
+//   console.log("TEACHER = SIR ABRAR HUSSAIN");
+// } else if (classes == "Graphic") {
+//   console.log("COURSE OUTLINE");
+// }
+// else {
+//   console.log("I DON'T KNOW");
+// }
+
+// //TASK 15........
+// let x = 5, y = 9;
+// console.log((2*(x*y)) + (8*x) + (2*(y**2)));
+
+// //TASK 16......
+// let a= 10, b= 10, c=9;
+// console.log((a**2) + (b**2) + (c**2) + (2*a*b) + (2*b*c) + (a*b*c)**2);
+
+//TASK 17......................
+// let button = "On";
+// if (button == "On") {
+//   console.log("Fan Is On");
+// } else {
+//   console.log("Fan Is Off");
+// }
+
+//TASK 18................
+// let vehicle = "Train";
+// if (vehicle == "Car") {
+//   console.log("It's too slow");
+// } else if (vehicle == "Train") {
+//   console.log("It's too long.");
+// } else if ((vehicle = "Aeroplane")) {
+//   console.log("You can fly");
+// } else {
+//   console.log("You should wal on your feet");
+// }
+
+// let num1 = -100,
+//   x = 98;
+// let r = num1 + x;
+// if (r > 200) {
+//   console.log("Very good");
+// } else if (r < 5) {
+//   console.log("Very bad");
+// } else {
+//   console.log("Nothing");
+// }
+
+// let a = "Abrar";
+// if (a == "Abrar") {
+//     console.log("You Are A Teacher, Sir", a);
+// } else {
+//     console.log("You Are A Student,", a);
+// }
