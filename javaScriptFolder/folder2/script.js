f2a146bf1c70ff283477667c0eed00fa548090a8
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-04-19 17:04:34 +0500

    Add files via upload

---
+++ javaScriptFolder/folder2/script.js
@@ -0,0 +1,20 @@
+let myName = "KIRAN SHAMS";
+let fName = "SHAMS-UD-DIN";
+
+console.log("MY NAME IS", myName, "AND MY FATHER's NAME IS", fName);
+
+// let x = 9;
+// let y = 2;
+// let z = x % y;
+// console.log("THE RESULT IS",z);
+// // console.log("MULTIPLICATION OF", x, "AND", y, "IS =", z);
+
+let x = 2,
+  y = 3;
+
+console.log((x + y) ** 2);
+console.log(x ** 2 + 3 * y);
+console.log(x + 2 * x);
+console.log(y + x * y + y * y);
+console.log(x - 2 * y + x * x);
+console.log(x / y + 2 * y);
