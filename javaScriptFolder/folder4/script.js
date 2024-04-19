03a586ad896da5955bd9bf078d78b1b4d865e0b4
Kiran Shams <160856436+kiranShamsHere@users.noreply.github.com>
2024-04-19 17:07:48 +0500

    Add files via upload

---
+++ javaScriptFolder/folder4/script.js
@@ -0,0 +1,9 @@
+// let count = 0;
+for (let count = 1; count <= 10; count++) {
+    console.log("I Am Sorry", count);
+}
+
+console.log("\n")
+for (let count = 10; count >= 1; count--) {
+    console.log("I Am Sorry", count);
+}
